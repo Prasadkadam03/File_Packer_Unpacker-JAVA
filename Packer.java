3dc16365de7d61c5b7a7ed5b80215154d2229f86
Prasadkadam03 <prasadkadam29503@gmail.com>
2024-01-14 23:34:12 +0530

    Packing

--- Packer.java
+++ Packer.java
@@ -1,14 +1,18 @@
 import java.util.*;
 import java.io.*;
 
-class Program236
+class Packer
 {
     public static void main(String arg[]) throws Exception
     {
+        System.out.println("---------------------------------------------------");
+        System.out.println("---------- Prasad's File Packer Unpacker ----------");
+        System.out.println("---------------------------------------------------");
+
         Scanner sobj = new Scanner(System.in);
         boolean bret = false;
 
-        System.out.println("Enter the name of Directory : ");
+        System.out.println("Enter the name of Directory which contains all files that you want to pack: ");
         String DirectoryName = sobj.nextLine();
 
         System.out.println("Enter the name of packed file that you want to create : ");
@@ -23,13 +27,13 @@ class Program236
             return;
         }
 
+        System.out.println("Packed file gets succesfully created in current directory.");
+
         File fobj = new File(DirectoryName);
 
         bret = fobj.isDirectory();
         if(bret == true)
         {
-            System.out.println("Directory is present");
-
             File Arr[] = fobj.listFiles();
             System.out.println("Number of files in the directory are : "+Arr.length);
 
@@ -40,28 +44,44 @@ class Program236
             int iRet = 0;
             byte Buffer[] = new byte[1024];
             
+            System.out.println("Packing activity started...");
+
             // Travel Directory
             for(int i = 0; i < Arr.length; i++)
             {
+                // Create header
                 Header = Arr[i].getName() + " " + Arr[i].length();
+                System.out.println("File packed with the name : "+Arr[i].getName());
 
+                // Add extra white spaces at the end of header
                 for(int j = Header.length(); j < 100; j++)
                 {
                     Header = Header + " ";
                 }
 
+                // Convert string header into byte array
                 byte hArr[] = Header.getBytes();
 
+                // Write header into packed file
                 fcombine.write(hArr,0,100);
 
+                // To read the file from duirectory
                 FileInputStream fiobj = new FileInputStream(Arr[i]);
 
+                // write the data into packed file after header
                 while((iRet = fiobj.read(Buffer)) != -1 )
                 {
                     fcombine.write(Buffer,0,iRet);
                 }
                 fiobj.close();
             }
+
+            System.out.println("Packing activity completed..");
+            System.out.println("Total file packed succesfully : "+Arr.length);
+
+            System.out.println("-----------------------------------------------------");
+            System.out.println("- Thank you for using Prasad's File Packer Unpacker -");
+            System.out.println("-----------------------------------------------------");
         }
         else 
         {
@@ -69,4 +89,3 @@ class Program236
         }
     }
 }
-
